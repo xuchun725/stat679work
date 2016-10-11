de144090dd133de392178dc5d0d55a475ae6cd29
Xuchun Liu <xliu588@wisc.edu>
2016-10-11 09:54:28 -0500

    modify hw1 ex1&ex2 based on professor's feedback
    - change "$var" to ${var} in normalizedFileNames.sh and summarizeSNaQres.sh
    - use more grep and extended regular expression in summarizeSNaQres.sh instead of cut.
    - update the above changes in hw1/readme.md

--- hw1/summarizeSNaQres.sh
+++ hw1/summarizeSNaQres.sh
@@ -1,10 +1,10 @@
 # Create a table in csv format to summarize the the results of all these analysis in log/ and out/.
 # 1 row per analysis and 3 columns
 # Usage: in `stat679work/hw1`, run `bash summarizeSNaQres.sh`
-echo "analysis, h, CPUtime" > summary.csv
-for filename in $(ls -l log | grep -oE "\w*\.log" | cut -f1 -d'.')
+echo "analysis,h,CPUtime" > summary.csv
+for filename in $(ls -l log | grep -oE "\w*\.log" | grep -oE "\w*[^(.log)]")
 do
-  h=$(grep -oE "hmax = [0-9]+" log/"$filename".log | cut -f3 -d' ')
-  CPUtime=$(grep "Elapsed time" out/"$filename".out | cut -f4 -d' ')
-  echo "$filename, $h, $CPUtime" >> summary.csv
+  h=$(grep -oE "hmax = [0-9]+" log/"${filename}".log | grep -oE "[0-9]+")
+  CPUtime=$(grep "Elapsed time" out/"${filename}".out | grep -oE "[0-9]+.[0-9]+")
+  echo "$filename,$h,$CPUtime" >> summary.csv
 done
